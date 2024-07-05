6c38f11d15676511697bbfa2cde1ac9e1e3d77b6
zhongzd52517 <zhongzd52517@hundsun.com>
2024-07-05 17:09:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<!--
+ * @Description: 
+ * @Author: zhongzd
+ * @Date: 2024-07-04 19:04:54
+ * @LastEditors: zhongzd
+ * @LastEditTime: 2024-07-05 10:03:13
+ * @FilePath: \zzd\vue3-PC_temp\src\App.vue
+-->
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!"></HelloWorld>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  max-height: 100vh;
+  line-height: 1.5;
+}
+.logo {
+  width: 100px;
+  position: relative;
+  display: block;
+  margin: 0 auto 2rem;
+}
+nav {
+  width: 100%;
+  margin-top: 2rem;
+  font-size: 12px;
+  text-align: center;
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (width >= 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+  header .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    place-items: flex-start;
+  }
+  nav {
+    padding: 1rem 0;
+    margin-top: 1rem;
+    margin-left: -1rem;
+    font-size: 1rem;
+    text-align: left;
+  }
+}
+</style>
